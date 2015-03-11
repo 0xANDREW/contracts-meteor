efc49e265e9c9fea1a0f88ab6ee1d307e1d3846f
Andrew Muro <andrew.t.muro@gmail.com>
2015-03-11 18:29:39 -0400

    init

---
+++ contracts-meteor.js
@@ -0,0 +1,89 @@
+Contracts = new Mongo.Collection('contracts');
+
+if (Meteor.isClient){
+    Session.set('current_template', 'form');
+
+    Template.form.events({
+        'click #generate': function(e){
+            Session.set('current_template', 'contract');
+        },
+
+        'click #save': function(e){
+            var attrs = {};
+
+            _.each(Template.instance().$('input'), function(el){
+                var $el = $(el);
+
+                if ($el.is(':checkbox')){
+                    attrs[$el.attr('id')] = $el.prop('checked');
+                }
+                else {
+                    attrs[$el.attr('id')] = $el.val();
+                }
+            });
+
+            Contracts.insert(attrs);
+        },
+
+        'click #load': function(e){
+            Session.set('current_template', 'load');            
+        }
+    });
+
+    Template.form.helpers({
+        'contract': function(){
+            var cid = Session.get('cid');
+            
+            if (cid){
+                return Contracts.findOne({ _id: cid });
+            }
+            
+            return {};            
+        }
+    });
+
+    Template.contract.rendered = function(){
+        window.print();
+    };
+
+    Template.contract.events({
+        'click': function(e){
+            Session.set('current_template', 'form');
+        }
+    });
+
+    Template.load.helpers({
+        contracts: function(){
+            return Contracts.find({});
+        }
+    });
+
+    Template.load.events({
+        'click .load': function(e){
+            var cid = $(e.target).parents('.row').attr('id');
+
+            Session.set('cid', cid);
+            Session.set('current_template', 'form');
+        },
+
+        'click #back': function(e){
+            Session.set('current_template', 'form');
+        },
+
+        'click .delete': function(e){
+            console.log('delete');
+        }
+    });
+
+    Template.body.helpers({
+        current_template: function(){
+            return Session.get('current_template');
+        }
+    });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
