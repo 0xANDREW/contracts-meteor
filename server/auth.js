a19045aa8ffc24a1c5aeff71a2f6062d2af51716
Andrew Muro <andrew.t.muro@gmail.com>
2015-04-29 11:00:32 -0400

    remove auth files

--- server/auth.js
+++
@@ -1,7 +0,0 @@
-// if (process.env.USER && process.env.PASS){
-//     var basicAuth = new HttpBasicAuth(process.env.USER, process.env.PASS);
-//     basicAuth.protect();
-// }
-// else {
-//     throw("server/auth.env doesn't exist");
-// }
